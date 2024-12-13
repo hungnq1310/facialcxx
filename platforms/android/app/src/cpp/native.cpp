5723cf3f711fee29edcff01b2a8b6347a98b0aee
hungnq131002 <hungnq131002@gmail.com>
2024-12-13 10:19:32 +0700

    chores: remove bitmap, change to use share allocator and env

--- platforms/android/app/src/cpp/native.cpp
+++ platforms/android/app/src/cpp/native.cpp
@@ -27,107 +27,14 @@ extern "C" {
 #endif
 
 
-// BlazeFace* blazefacePipeline;
 YoloV7* yoloPipeline;
 Spoofing* SpoofingPipeline;
-
-
-
-// Convert the bitmap to OpenCV Mat
-// Reference https://github.com/opencv/opencv/blob/17234f82d025e3bbfbf611089637e5aa2038e7b8/modules/java/generator/src/cpp/utils.cpp
-void bitmapToMat(JNIEnv *env, jobject bitmap, Mat &dst, jboolean needUnPremultiplyAlpha) {
-    AndroidBitmapInfo info;
-    void *pixels = 0;
-
-    try {
-        CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info) >= 0);
-        CV_Assert(info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
-                  info.format == ANDROID_BITMAP_FORMAT_RGB_565);
-        CV_Assert(AndroidBitmap_lockPixels(env, bitmap, &pixels) >= 0);
-        CV_Assert(pixels);
-        dst.create(info.height, info.width, CV_8UC4);
-        if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888) {
-            Mat tmp(info.height, info.width, CV_8UC4, pixels);
-            if (needUnPremultiplyAlpha) cvtColor(tmp, dst, COLOR_mRGBA2RGBA);
-            else tmp.copyTo(dst);
-        } else {
-            // info.format == ANDROID_BITMAP_FORMAT_RGB_565
-            Mat tmp(info.height, info.width, CV_8UC2, pixels);
-            cvtColor(tmp, dst, COLOR_BGR5652RGBA);
-        }
-        AndroidBitmap_unlockPixels(env, bitmap);
-        return;
-    } catch (const cv::Exception &e) {
-        AndroidBitmap_unlockPixels(env, bitmap);
-        jclass je = env->FindClass("java/lang/Exception");
-        env->ThrowNew(je, e.what());
-        return;
-    } catch (...) {
-        AndroidBitmap_unlockPixels(env, bitmap);
-        jclass je = env->FindClass("java/lang/Exception");
-        env->ThrowNew(je, "Unknown exception in JNI code {nBitmapToMat}");
-        return;
-    }
-}
-
-// Convert a mat to Bitmap
-// Reference https://github.com/opencv/opencv/blob/17234f82d025e3bbfbf611089637e5aa2038e7b8/modules/java/generator/src/cpp/utils.cpp
-void matToBitmap(JNIEnv *env, Mat src, jobject bitmap, jboolean needPremultiplyAlpha) {
-    AndroidBitmapInfo info;
-    void *pixels = 0;
-
-    try {
-        CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info) >= 0);
-        CV_Assert(info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
-                  info.format == ANDROID_BITMAP_FORMAT_RGB_565);
-        CV_Assert(src.dims == 2);
-        CV_Assert(info.height == (uint32_t) src.rows);
-        CV_Assert(info.width == (uint32_t) src.cols);
-        CV_Assert(src.type() == CV_8UC1 || src.type() == CV_8UC3 || src.type() == CV_8UC4);
-        CV_Assert(AndroidBitmap_lockPixels(env, bitmap, &pixels) >= 0);
-        CV_Assert(pixels);
-        if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888) {
-            Mat tmp(info.height, info.width, CV_8UC4, pixels);
-            if (src.type() == CV_8UC1) {
-                cvtColor(src, tmp, COLOR_GRAY2RGBA);
-            } else if (src.type() == CV_8UC3) {
-                cvtColor(src, tmp, COLOR_RGB2RGBA);
-            } else if (src.type() == CV_8UC4) {
-                if (needPremultiplyAlpha) cvtColor(src, tmp, COLOR_RGBA2mRGBA);
-                else src.copyTo(tmp);
-            }
-        } else {
-            // info.format == ANDROID_BITMAP_FORMAT_RGB_565
-            Mat tmp(info.height, info.width, CV_8UC2, pixels);
-            if (src.type() == CV_8UC1) {
-                cvtColor(src, tmp, COLOR_GRAY2BGR565);
-            } else if (src.type() == CV_8UC3) {
-                cvtColor(src, tmp, COLOR_RGB2BGR565);
-            } else if (src.type() == CV_8UC4) {
-                cvtColor(src, tmp, COLOR_RGBA2BGR565);
-            }
-        }
-        AndroidBitmap_unlockPixels(env, bitmap);
-        return;
-    } catch (const cv::Exception &e) {
-        AndroidBitmap_unlockPixels(env, bitmap);
-        jclass je = env->FindClass("java/lang/Exception");
-        env->ThrowNew(je, e.what());
-        return;
-    } catch (...) {
-        AndroidBitmap_unlockPixels(env, bitmap);
-        jclass je = env->FindClass("java/lang/Exception");
-        env->ThrowNew(je, "Unknown exception in JNI code {nMatToBitmap}");
-        return;
-    }
-}
 
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_ai_spoofing_TensorUtils_initModel(
     JNIEnv *env, jclass clazz, jobject assetManager,
-    // jstring blazeFacePath,
     jstring yoloPath,
     jstring extractorPath,
     jstring embedderPath
@@ -158,19 +65,19 @@ Java_ai_spoofing_TensorUtils_initModel(
     c["inter_ops_threads"] = 1;
     c["intra_ops_threads"] = 1;
     c["graph_optimization_level"] = 1;
+    c["session.use_env_allocators"] = true;
 
     // Load the model with share env and allocator
-    std::shared_ptr<Ort::Env> ortEnv = std::make_shared<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "test");
+    shared_ptr<Ort::Env> ortEnv = make_shared<Ort::Env>(ORT_LOGGING_LEVEL_WARNING, "test");
+    ortEnv->CreateAndRegisterAllocator(Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault), {});
 
     std::shared_ptr<Model> yoloModel = Model::create(yoloString, ortEnv, c, providers, false);
     std::shared_ptr<Model> extractorModel = Model::create(extractorString, ortEnv, c, providers, false);
     std::shared_ptr<Model> embedderModel = Model::create(embedString, ortEnv, c, providers, false);
 
     // Assign pointers
-    // blazefacePipeline = new BlazeFace(blazefaceModel);
     yoloPipeline = new YoloV7(yoloModel);
     SpoofingPipeline = new Spoofing(extractorModel, embedderModel);
-
     //DONE
 }
 
