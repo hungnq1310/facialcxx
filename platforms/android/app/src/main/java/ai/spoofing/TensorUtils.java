26e2b0a5d54184212eca1c95433f8de9c437d10e
hungnq131002 <hungnq131002@gmail.com>
2024-12-15 20:12:51 +0700

    fix: change bitmap to bytes instead

--- platforms/android/app/src/main/java/ai/spoofing/TensorUtils.java
+++ platforms/android/app/src/main/java/ai/spoofing/TensorUtils.java
@@ -6,19 +6,12 @@ import android.util.Log;
 
 public class TensorUtils {
     // inference in C++
-    // public static native void initModel(
-    //     AssetManager assetManager,
-    //     String blazeFacePath,
-    //     String extractorPath,
-    //     String embedderPath
-    // );
     public static native void initModel(
         AssetManager assetManager,
         String yoloPath,
         String extractorPath,
         String embedderPath
     );
-//    public static native float[][] checkspoof(Bitmap bitmap);
-    public static  native DetectionResult[] checkspoof(Bitmap bitmap);
+    public static  native DetectionResult[] checkspoof(byte[] imageArray);
 }
 
