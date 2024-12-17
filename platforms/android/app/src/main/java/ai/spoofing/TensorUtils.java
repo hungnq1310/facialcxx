c7b60d5ee4ddd4e2126ad12c3c3d323bbc47af36
hungnq131002 <hungnq131002@gmail.com>
2024-12-17 15:03:16 +0700

    chore: clean stuff

--- platforms/android/app/src/main/java/ai/spoofing/TensorUtils.java
+++ platforms/android/app/src/main/java/ai/spoofing/TensorUtils.java
@@ -5,20 +5,12 @@ import android.graphics.Bitmap;
 import android.util.Log;
 
 public class TensorUtils {
-    // inference in C++
-    // public static native void initModel(
-    //     AssetManager assetManager,
-    //     String blazeFacePath,
-    //     String extractorPath,
-    //     String embedderPath
-    // );
     public static native void initModel(
         AssetManager assetManager,
         String yoloPath,
         String extractorPath,
         String embedderPath
     );
-//    public static native float[][] checkspoof(Bitmap bitmap);
-    public static  native DetectionResult[] checkspoof(Bitmap bitmap);
+    public static  native DetectionResult[] checkspoof(Bitmap imageData);
 }
 
